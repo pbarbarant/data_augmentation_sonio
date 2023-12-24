a10fccf5754617fc4a6bb3591f0de49960c6bfaa
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2023-12-25 00:17:10 +0100

    Update training parameters in textual_inversion_healthy/train.sh

--- models/textual_inversion_healthy/train.sh
+++ models/textual_inversion_healthy/train.sh
@@ -9,15 +9,14 @@ python textual_inversion.py \
     --initializer_token="heart" \
     --resolution=512 \
     --train_batch_size=1 \
-    --gradient_accumulation_steps=4 \
-    --resolution=512 \
-    --max_train_steps=3000 \
+    --max_train_steps=50000 \
     --num_vectors=8 \
     --learning_rate=5.0e-03 \
     --scale_lr \
     --lr_scheduler="constant" \
     --lr_warmup_steps=0 \
+    --save_steps=10000 \
     --output_dir="textual_inversion_output" \
-    --validation_prompt="A <cardiac-healthy> train" \
-    --num_validation_images=4 \
-    --validation_steps=100
+    # --validation_prompt="A <cardiac-healthy> train" \
+    # --num_validation_images=4 \
+    # --validation_steps=100
