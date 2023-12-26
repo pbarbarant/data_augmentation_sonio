186851b9c4ec264c79e1f6ec95055ad6a77cec10
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2023-12-26 04:12:30 +0100

    Add metadata generation for separating script

--- data/separate_modalities.py
+++ data/separate_modalities.py
@@ -1,23 +1,27 @@
 # %%
 import glob
+import os
 from pathlib import Path
 import json
+import pandas as pd
 import shutil
 from tqdm import tqdm
+from datasets import load_dataset
 
 source_folder = Path(
     "/data/parietal/store3/work/pbarbara/data_augmentation_sonio/data/processed/"
 )
 target_folder = Path(
-    "/data/parietal/store3/work/pbarbara/data_augmentation_sonio/data/processed_by_modality/"
+    "/data/parietal/store3/work/pbarbara/data_augmentation_sonio/data/processed_by_modality/train/"
 )
 modalities_path = Path(
     "/data/parietal/store3/work/pbarbara/data_augmentation_sonio/data/raw/data_M2_healthy/data_constructor_healthy.json"
 )
 
-assert source_folder.exists(), "source_folder does not exist"
-assert target_folder.exists(), "target_folder does not exist"
-assert modalities_path.exists(), "modalities_path does not exist"
+assert source_folder.exists(), "Source folder does not exist"
+assert modalities_path.exists(), "JSON file with modalities does not exist"
+if not os.path.exists(target_folder):
+    os.makedirs(target_folder)
 
 # Load modalities
 modalities_dict = json.load(
@@ -28,6 +32,12 @@ modalities_dict = json.load(
 )
 
 filenames_list = glob.glob(str(source_folder / "*.jpg"))
+
+# Create json file with filenames and modalities
+dict_names_captions = {
+    "file_name": [],
+    "caption": [],
+}
 
 # Replace identifiers by file paths and assign unique number for each image according to modality
 number_modality = {
@@ -41,5 +51,24 @@ for filename in tqdm(filenames_list):
     output_filename = f"{modality}{number_modality[modality]}.jpg"
     number_modality[modality] += 1
     target_path = target_folder / output_filename
+    # Add modality to dataframe
+    dict_names_captions["file_name"].append(output_filename)
+    dict_names_captions["caption"].append(
+        f"A healthy cardiac image from a {modality} scanner"
+    )
     # Copy file to target folder
     shutil.copy(filename, target_path)
+
+# Create dataframe
+df = pd.DataFrame.from_dict(dict_names_captions)
+# Save to csv
+df.to_csv(target_folder / "metadata.csv", index=False)
+
+# Create a hf dataset
+dataset = load_dataset(str(target_folder))
+# Save to disk
+dataset.save_to_disk(
+    str(
+        "/data/parietal/store3/work/pbarbara/data_augmentation_sonio/data/hf_dataset_healthy"
+    )
+)
