3957922e7e2c1e14e800bc224b53ef5dc7b5f906
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2024-02-08 02:51:47 +0100

    Add fetch_unhealthy_dataset function to download and unzip unhealthy data

--- data/fetcher.py
+++ data/fetcher.py
@@ -23,8 +23,26 @@ def fetch_healthy_dataset(url, output):
     print("Data downloaded and unzipped successfully!")
 
 
+def fetch_unhealthy_dataset(url, output):
+    """Download and unzip unhealthy data from Google Drive."""
+    if not output.exists():
+        output.mkdir(parents=True)
+    # Download data
+    os.system(f"gdown {url} -O data/raw/data_unhealthy.zip")
+    # Check if data is downloaded
+    assert os.path.isfile("data/raw/data_unhealthy.zip")
+    # Unzip data
+    os.system("unzip data/raw/data_unhealthy -d data/raw/")
+    # Remove zip file and __MACOSX folder
+    os.remove("data/raw/data_unhealthy.zip")
+    os.system("rm -rf data/raw/__MACOSX")
+    print("Data downloaded and unzipped successfully!")
+
+
 if __name__ == "__main__":
     output = Path("data/raw")
 
     # Fetch healthy data
-    fetch_healthy_dataset(config["datasets"]["url_data_normal"], output)
+    # fetch_healthy_dataset(config["datasets"]["url_data_normal"], output)
+    # Fetch pathological data
+    fetch_unhealthy_dataset(config["datasets"]["url_data_unhealthy"], output)
