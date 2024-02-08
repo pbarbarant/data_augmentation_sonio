87b65de91aa016922ad55dc786b17535f604a6ce
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2024-02-08 03:04:29 +0100

    Update launch_slurm.sh to allocate GPU resources and set task parameters

--- models/sd_lora/launch_slurm.sh
+++ models/sd_lora/launch_slurm.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 srun --job-name=accelerate_multi-node \
     --partition=gpu \
-    --gres=gpu:1 \
+    --gres=gpu:ampere:1 \
+    --nodes=1 \
+    --ntasks-per-node=1 \
+    --cpus-per-task=32 \
     --time=70:00:00 \
     bash train.sh
