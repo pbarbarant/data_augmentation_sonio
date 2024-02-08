959aa032205367573ae5eb09ba39a8eef7ba869e
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2024-02-08 03:04:34 +0100

    Update training parameters in train.sh script

--- models/sd_lora/train.sh
+++ models/sd_lora/train.sh
@@ -1,7 +1,7 @@
 export MODEL_NAME="stabilityai/sd-turbo"
+# export MODEL_PATH="/data/parietal/store3/work/pbarbara/data_augmentation_sonio/models/sd_lora/sd_lora_output/pytorch_lora_weights.safetensors"
 export OUTPUT_DIR="/data/parietal/store3/work/pbarbara/data_augmentation_sonio/models/sd_lora/sd_lora_output"
 export DATASET_NAME="/data/parietal/store3/work/pbarbara/data_augmentation_sonio/data/processed_by_modality/train"
-
 accelerate launch --gpu_ids 0 train_text_to_image_lora.py \
     --pretrained_model_name_or_path=$MODEL_NAME \
     --train_data_dir=$DATASET_NAME \
@@ -9,13 +9,14 @@ accelerate launch --gpu_ids 0 train_text_to_image_lora.py \
     --resolution=512 \
     --train_batch_size=1 \
     --gradient_accumulation_steps=4 \
-    --max_train_steps=15000 \
+    --max_train_steps=20000 \
     --learning_rate=1e-04 \
     --max_grad_norm=1 \
     --lr_scheduler="cosine" \
     --lr_warmup_steps=0 \
     --output_dir=${OUTPUT_DIR} \
     --checkpointing_steps=1000 \
+    --resume_from_checkpoint latest \
     --image_column="image" \
     --caption_column="caption" \
     --report_to="wandb"
