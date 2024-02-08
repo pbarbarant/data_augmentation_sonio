2eb816b84d5d318d5b5f736bb9f0a4fa04cdc0b9
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2024-02-08 02:53:03 +0100

    Update synthetic_data_folder and prompt for pathological cardiac image generation

--- src/echo-augmentation-sd/generation_script.py
+++ src/echo-augmentation-sd/generation_script.py
@@ -6,7 +6,8 @@ from pathlib import Path
 NUM_SAMPLES = 100
 
 # Target folder
-synthetic_data_folder = Path("../../data/synthetic_healthy")
+# synthetic_data_folder = Path("../../data/synthetic_healthy")
+synthetic_data_folder = Path("../../data/synthetic_patho")
 if not synthetic_data_folder.exists():
     synthetic_data_folder.mkdir()
 
@@ -22,12 +23,13 @@ assert path.exists(), "File not found"
 # Load the textual inversion embeddings
 pipeline.load_lora_weights(path, weight_name="pytorch_lora_weights.safetensors")
 
-modalities = ["Philipps", "GE", "Samsung"]
+modalities = ["Philipps", "General-Electric", "Samsung"]
 for modality in modalities:
     for i in range(NUM_SAMPLES):
         print(f"Generating image {i}/{NUM_SAMPLES} for {modality} scanner")
         # Generate the image
-        prompt = f"A healthy cardiac image from a {modality} scanner"
+        # prompt = f"A healthy cardiac image from a {modality} scanner"
+        prompt = f"A pathological cardiac image from a {modality} scanner"
         sample = pipeline(
             prompt,
             num_inference_steps=100,
