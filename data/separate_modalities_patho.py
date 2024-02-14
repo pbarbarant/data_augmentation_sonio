ee7d6f523b117ade74a9692b3ae57a60c4e24da1
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2024-02-14 20:43:18 +0100

    Refactor file paths and update captions in separate_modalities_patho.py

--- data/separate_modalities_patho.py
+++ data/separate_modalities_patho.py
@@ -15,7 +15,7 @@ target_folder = Path(
     "/data/parietal/store3/work/pbarbara/data_augmentation_sonio/data/processed_by_modality/patho/"
 )
 modalities_path = Path(
-    "/data/parietal/store3/work/pbarbara/data_augmentation_sonio/data/raw/data_M2_patho/data_constructor_patho.json"
+    "/data/parietal/store3/work/pbarbara/data_augmentation_sonio/data/raw/data_patho/data_constructor.json"
 )
 
 assert source_folder.exists(), "Source folder does not exist"
@@ -26,17 +26,20 @@ if not os.path.exists(target_folder):
 # Load modalities
 modalities_dict = json.load(
     open(
-        "/data/parietal/store3/work/pbarbara/data_augmentation_sonio/data/raw/data_M2_patho/data_constructor_patho.json",
+        "/data/parietal/store3/work/pbarbara/data_augmentation_sonio/data/raw/data_patho/data_constructor.json",
         "r",
     )
 )
 # Load pathologies names
 patho_dict = json.load(
     open(
-        "/data/parietal/store3/work/pbarbara/data_augmentation_sonio/data/raw/data_M2_patho/data_pathology.json",
+        "/data/parietal/store3/work/pbarbara/data_augmentation_sonio/data/raw/data_patho/data_pathology.json",
         "r",
     )
 )
+assert len(modalities_dict) > 0, "No modalities found"
+assert len(patho_dict) > 0, "No pathologies found"
+
 
 filenames_list = glob.glob(str(source_folder / "*.jpg"))
 
@@ -52,23 +55,28 @@ number_modality = {
     "Philips": 0,
     "Samsung": 0,
 }
+
 for filename in tqdm(filenames_list):
     identifier = filename.split("/")[-1].split(".")[0]
     modality = modalities_dict[identifier]
     patho = patho_dict[identifier]
+    if patho == "Cardiomégalie":
+        patho = "Cardiomegaly"
+    else:
+        patho = "Interventricular communication"
+
     output_filename = f"{modality}{number_modality[modality]}.jpg"
     number_modality[modality] += 1
     target_path = target_folder / output_filename
+
     # Add modality to dataframe
-    dict_names_captions["file_name"].append(output_filename)
     if modality == "GE":
-        dict_names_captions["caption"].append(
-            f"A pathological cardiac image from a General-Electric scanner with {patho}"
-        )
-    else:
-        dict_names_captions["caption"].append(
-            f"A pathological cardiac image from a {modality} scanner with {patho}"
-        )
+        modality = "General-Electric"
+
+    dict_names_captions["file_name"].append(output_filename)
+    dict_names_captions["caption"].append(
+        f"A pathological cardiac image with {patho} from a {modality} scanner"
+    )
     # Copy file to target folder
     shutil.copy(filename, target_path)
 
