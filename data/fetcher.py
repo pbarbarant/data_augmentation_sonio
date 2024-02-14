d19ca59921711529bf58b3469199ebc940e5d6fd
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2024-02-14 20:41:51 +0100

    Refactor data fetching code

--- data/fetcher.py
+++ data/fetcher.py
@@ -28,21 +28,23 @@ def fetch_unhealthy_dataset(url, output):
     if not output.exists():
         output.mkdir(parents=True)
     # Download data
-    os.system(f"gdown {url} -O data/raw/data_unhealthy.zip")
-    # Check if data is downloaded
-    assert os.path.isfile("data/raw/data_unhealthy.zip")
-    # Unzip data
-    os.system("unzip data/raw/data_unhealthy -d data/raw/")
-    # Remove zip file and __MACOSX folder
-    os.remove("data/raw/data_unhealthy.zip")
+    os.system(f"gdown {url} -O data/raw/data_patho --folder ")  # --remaining-ok
+
+    # Check if the folder is not empty
+    assert os.path.isdir("data/raw/data_patho")
+
+    # Remove __MACOSX folder
     os.system("rm -rf data/raw/__MACOSX")
     print("Data downloaded and unzipped successfully!")
 
 
 if __name__ == "__main__":
-    output = Path("data/raw")
+    # output = Path("data/raw")
+    output = Path("data/raw/data_patho")
 
     # Fetch healthy data
     # fetch_healthy_dataset(config["datasets"]["url_data_normal"], output)
+
     # Fetch pathological data
+    print(config["datasets"]["url_data_patho"])
     fetch_unhealthy_dataset(config["datasets"]["url_data_patho"], output)
