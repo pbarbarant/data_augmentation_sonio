13769352d18ffb4a0a38138670aa675bfccf696f
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2024-02-14 20:43:25 +0100

    Update GPU type in launch_slurm.sh script

--- models/sd_lora/launch_slurm.sh
+++ models/sd_lora/launch_slurm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 srun --job-name=accelerate_multi-node \
     --partition=gpu \
-    --gres=gpu:ampere:1 \
+    --gres=gpu:volta \
     --nodes=1 \
     --ntasks-per-node=1 \
     --cpus-per-task=32 \
     --time=70:00:00 \
-    bash train.sh
+    bash models/sd_lora/train.sh
